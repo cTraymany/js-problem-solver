1eeda0c9af05cc470a150b6d3f21b4b67d06d7b6
cTraymany <chindalatht@gmail.com>
2020-10-28 11:08:39 -0700

    add fetch for submitting new problem

--- js-problem-solver-backend/app/controllers/problems_controller.rb
+++ js-problem-solver-backend/app/controllers/problems_controller.rb
@@ -2,4 +2,8 @@ class ProblemsController < ApplicationController
     def index
         render json: ProblemSerializer.new(Problem.all)
     end
+
+    def create
+        
+    end
 end
--- js-problem-solver-frontend/src/problem.js
+++ js-problem-solver-frontend/src/problem.js
@@ -13,18 +13,24 @@ fetch("http://localhost:3000/problems")
         }
     })
 
-
-// function renderProblems() {
-//     const div = document.getElementById("problems")
-//     const ul = document.createElement("ul")
-//     div.appendChild(ul)
-    
-//     for (const problem of problems.data) {
-//         let li = document.createElement("li")
-//         li.innerText = problem.attributes.title
-//         ul.appendChild(li)
-//     }
-// }
-
-
 // add event listener to show solutions on problem click
+
+const form = document.getElementById("problemForm")
+form.addEventListener("submit", submitForm)
+
+function submitForm() {
+    event.preventDefault()
+    const title = document.getElementById("problemTitle").value
+    const description = document.getElementById("problemDescription").value
+
+    const obj = {
+        method: "POST",
+        headers: {
+            "ContentType": "application/json",
+            "accept": "application/json"
+        },
+        body: JSON.stringify({problem: {title: title, description: description}})
+    }
+
+    fetch("http://localhost:3000/problems", obj)
+}
