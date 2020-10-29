615f45ab071526f7698f6daab945a89056381b24
cTraymany <chindalatht@gmail.com>
2020-10-28 18:20:48 -0700

    adds create and index actions to problems controller

--- js-problem-solver-backend/app/controllers/problems_controller.rb
+++ js-problem-solver-backend/app/controllers/problems_controller.rb
@@ -4,6 +4,17 @@ class ProblemsController < ApplicationController
     end
 
     def create
-        
+        problem = Problem.create(problem_params)
+        if problem.save
+            render json: ProblemSerializer.new(problem)
+        # else
+        #     flash: {message: "Please enter a title and description."}
+        end
+    end
+
+    private
+
+    def problem_params
+        params.require(:problem).permit(:title, :description)
     end
 end
