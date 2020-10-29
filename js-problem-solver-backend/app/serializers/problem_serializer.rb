2429d4dae9627aea388b90ebe5251bf6600b6de8
cTraymany <chindalatht@gmail.com>
2020-10-28 18:21:54 -0700

    creates problem serializer

--- js-problem-solver-backend/app/serializers/problem_serializer.rb
+++ js-problem-solver-backend/app/serializers/problem_serializer.rb
@@ -1,7 +1,6 @@
 class ProblemSerializer
   include FastJsonapi::ObjectSerializer
   attributes :title, :description
-  has_many :solutions
   attributes :solutions do |s|
     SolutionSerializer.new(s.solutions).as_json["data"]
   end
