009c134740cde95cce58e69a9fd4fc81475045e0
cTraymany <chindalatht@gmail.com>
2020-10-29 07:26:25 -0700

    creates Problem class and moves appropriate methods over from index.js

--- js-problem-solver-frontend/src/problem.js
+++ js-problem-solver-frontend/src/problem.js
@@ -1,36 +1,58 @@
-fetch("http://localhost:3000/problems")
-    .then(response => response.json())
-    .then(problems => {
-        // renderProblems()        
-        const div = document.getElementById("problems")
-        const ul = document.createElement("ul")
-        div.appendChild(ul)
-        
-        for (const problem of problems.data) {
-            let li = document.createElement("li")
-            li.innerText = problem.attributes.title
-            ul.appendChild(li)
-        }
-    })
+class Problem {
+    static all = []
 
-// add event listener to show solutions on problem click
-
-const form = document.getElementById("problemForm")
-form.addEventListener("submit", submitForm)
-
-function submitForm() {
-    event.preventDefault()
-    const title = document.getElementById("problemTitle").value
-    const description = document.getElementById("problemDescription").value
-
-    const obj = {
-        method: "POST",
-        headers: {
-            "ContentType": "application/json",
-            "accept": "application/json"
-        },
-        body: JSON.stringify({problem: {title: title, description: description}})
+    constructor(problem) {
+        this.title = problem.attributes.title;
+        this.description = problem.attributes.description
+        this.id = problem.id
+        Problem.all.push(this)
     }
 
-    fetch("http://localhost:3000/problems", obj)
+    renderProblem() {
+        const div = document.getElementById("problemsContainer")
+        let p = document.createElement("p")
+        p.innerText = this.title
+        div.appendChild(p)
+    }
+    
+    static renderProblems() {
+        for (const problem of this.all) {
+            problem.renderProblem()
+        }
+    }
+    static fetchProblems() {
+        fetch("http://localhost:3000/problems")
+            .then(resp => resp.json())
+            .then(probs => {
+                for (const prob of probs.data) {
+                    let newProblem = new Problem(prob)
+                    // console.log(newProblem)
+                }
+                this.renderProblems()
+            })
+    }
+
+    static createProblem() {
+        event.preventDefault()
+        const title = document.getElementById("problemTitle").value
+        const description = document.getElementById("problemDescription").value
+        document.getElementById("problemTitle").value = ""
+        document.getElementById("problemDescription").value = ""
+    
+        const obj = {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              "Accept": "application/json"
+            },
+            body: JSON.stringify({problem: {title: title, description: description}})
+          }
+    
+        fetch("http://localhost:3000/problems", obj)
+          .then(resp => resp.json())
+          .then(jsObj => {
+            let newProblem = new Problem(jsObj.data)
+            newProblem.renderProblem()
+          })
+    }
 }
