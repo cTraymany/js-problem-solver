73b197d1cca1c34f5adacb2899fdacfe0ee4f5aa
cTraymany <chindalatht@gmail.com>
2020-10-29 12:18:06 -0700

    creates a solution class

---
+++ js-problem-solver-frontend/src/solution.js
@@ -0,0 +1,8 @@
+class Solution {
+    constructor(solution) {
+        this.content = solution.attributes.content
+        this.id = solution.id
+        this.problemId = solution.attributes.problem_id
+        // debugger
+    }
+}
