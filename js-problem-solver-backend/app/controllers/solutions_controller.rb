919728e9548b11198651c4b9f336d29e1b678d4c
cTraymany <chindalatht@gmail.com>
2020-10-29 22:15:19 -0700

    editor not reflecting changes previously pushed -- pushing again

--- js-problem-solver-backend/app/controllers/solutions_controller.rb
+++ js-problem-solver-backend/app/controllers/solutions_controller.rb
@@ -1,8 +1,8 @@
 class SolutionsController < ApplicationController
     def create
-        binding.pry
         solution = Solution.create(solution_params)
-        if solution.valid
+
+        if solution.valid?
             render json: SolutionSerializer.new(solution)
         # else
             # flash message "enter valid content"
