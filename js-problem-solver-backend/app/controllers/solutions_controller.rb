4d4d1ea91d53ca8421f5ef222def32f7ea6fe72f
cTraymany <chindalatht@gmail.com>
2020-10-29 21:06:01 -0700

    defines create action for solution controller

--- js-problem-solver-backend/app/controllers/solutions_controller.rb
+++ js-problem-solver-backend/app/controllers/solutions_controller.rb
@@ -1,2 +1,11 @@
 class SolutionsController < ApplicationController
+    def create
+        binding.pry
+        solution = Solution.create(solution_params)
+        if solution.valid
+            render json: SolutionSerializer.new(solution)
+        # else
+            # flash message "enter valid content"
+        end
+    end
 end
