81b97679ac5b3c9469236e4131bc9b7f023baad5
cTraymany <chindalatht@gmail.com>
2020-10-30 11:23:47 -0700

    moves solution methods from problem class to solution class

--- js-problem-solver-frontend/src/solution.js
+++ js-problem-solver-frontend/src/solution.js
@@ -5,33 +5,64 @@ class Solution {
         this.problemId = solution.attributes.problem_id
     }
 
-    // static createSolution() {
-    //     const solutionForm = document.createElement("form")
-    //     // const container = document.getElementById("container")
-    //     const label = document.createElement("label")
-    //     const input = document.createElement("input")
-    //     const submit = document.createElement("input")
+    static createSolutionForm() {
+        const solutionForm = document.createElement("form")
+        const label = document.createElement("label")
+        const input = document.createElement("input")
+        const submit = document.createElement("input")
+        const ul = document.getElementById("solutionsUl")
+        const back = document.createElement("button")
+        back.innerHTML = "Back"
+        back.setAttribute("id", "cancel")
+        back.setAttribute("class", "btn-primary")
 
-    //     label.innerHTML = "Content"
-    //     input.setAttribute("type", "text")
-    //     input.setAttribute("id", "solutionContent")
-    //     submit.setAttribute("type", "submit")
+        solutionForm.setAttribute("id", "solutionForm")
+        label.innerHTML = "Give solution: "
+        input.setAttribute("type", "text")
+        input.setAttribute("id", "solutionContent")
+        submit.setAttribute("type", "submit")
+        submit.setAttribute("class", "btn-primary")
 
-    //     solutionForm.appendChild(label)
-    //     solutionForm.appendChild(input)
-    //     solutionForm.appendChild(submit)
-    //     container.appendChild(solutionForm)
+        solutionForm.appendChild(label)
+        solutionForm.appendChild(input)
+        solutionForm.appendChild(submit, document.createElement("br"))
+        container.appendChild(solutionForm)
+        container.appendChild(back)
 
-    //     solutionForm.addEventListener("submit", Solution.submitSolutionForm)
-    // }
+        back.addEventListener("click", () => location.reload())
 
-    // static submitSolutionForm() {
-    //     event.preventDefault()
-    //     // debugger
-    //     // 
-    //     const content = document.getElementById("solutionContent").value
-    //     document.getElementById("solutionContent").value = ""
+    }
+
+    static submitSolution() {
+        event.preventDefault()
+        const content = document.getElementById("solutionContent").value
+        const problemId = this.id
+
+        document.getElementById("solutionContent").value = ""
+
+        const obj = {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              "Accept": "application/json"
+            },
+            body: JSON.stringify({solution: {content: content, problem_id: problemId}})
+          }
     
+        fetch("http://localhost:3000/solutions", obj)
+          .then(resp => resp.json())
+          .then(jsObj => {
+            const newSolution = new Solution(jsObj.data)
+            const foundProb = Problem.all.find(prob => parseInt(prob.id) === newSolution.problemId)
+            
+            foundProb.solutions.push(newSolution)
 
-    // }
+            const showSolution = document.createElement("li")
+            showSolution.id = newSolution.id
+            showSolution.innerHTML = newSolution.content
+            document.getElementById("solutionsUl").appendChild(showSolution)
+
+          })
+    }
+
 }
