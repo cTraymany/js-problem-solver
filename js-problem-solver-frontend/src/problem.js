13063ffa59563b01602ac998a7a66ac7663a5c90
cTraymany <chindalatht@gmail.com>
2020-11-04 14:54:14 -0800

    moves creation of elements to appropriate forms

--- js-problem-solver-frontend/src/problem.js
+++ js-problem-solver-frontend/src/problem.js
@@ -12,10 +12,8 @@ class Problem {
     static renderProblemForm() {
         const container = document.getElementById("container")
         const formContainer = document.createElement("div")
-        const problemsContainer = document.createElement("div")
         const problemForm = document.createElement("form")
-
-        const h3 = document.createElement("h3")
+        
         const label1 = document.createElement("label")
         const label2 = document.createElement("label")
         const input1 = document.createElement("input")
@@ -23,10 +21,8 @@ class Problem {
         const submit = document.createElement("input")
         
         formContainer.setAttribute("id", "formContainer")
-        problemsContainer.setAttribute("id", "problemsContainer")
         problemForm.setAttribute("id", "problemForm")
-
-        h3.innerHTML = "Problems"
+        
         label1.innerHTML = "Title "
         input1.setAttribute("type", "text")
         input1.setAttribute("id", "problemTitle")
@@ -41,16 +37,14 @@ class Problem {
         problemForm.appendChild(label2)
         problemForm.appendChild(input2)
         problemForm.appendChild(submit, document.createElement("br"))
-
+        
         container.appendChild(formContainer)
-        container.appendChild(problemsContainer)
         
         formContainer.appendChild(problemForm)
-        formContainer.appendChild(h3)
-
+        
         problemForm.addEventListener("submit", Problem.createProblem)
     }
-
+    
     showSolutionsOnClick() {
         const container = document.getElementById("container")
         const problemH3 = document.createElement("h3")
@@ -58,7 +52,7 @@ class Problem {
         const ul = document.createElement("ul")
         const answers = document.createElement("h3")
         const back = document.createElement("button")
-
+        
         container.innerHTML = ""
         Solution.createSolutionForm()
         
@@ -72,7 +66,7 @@ class Problem {
         back.addEventListener("click", () => {
             const container = document.getElementById("container")
             container.innerHTML = ""
-
+            
             Problem.renderProblemForm()
             Problem.renderProblems()
         })
@@ -94,6 +88,16 @@ class Problem {
     }
     
     static renderProblems() {
+        const container = document.getElementById("container")
+        const problemsContainer = document.createElement("div")
+        const h3 = document.createElement("h3")
+
+        problemsContainer.setAttribute("id", "problemsContainer")
+        h3.innerHTML = "Problems"
+
+        problemsContainer.appendChild(h3)
+        container.appendChild(problemsContainer)
+
         for (const problem of this.all) {
             problem.renderProblem()
         }
@@ -101,8 +105,8 @@ class Problem {
     
     static fetchProblems() {
         fetch("http://localhost:3000/problems")
-            .then(resp => resp.json())
-            .then(probs => {
+        .then(resp => resp.json())
+        .then(probs => {
                 for (const prob of probs.data) {
                     new Problem(prob)
                 }
