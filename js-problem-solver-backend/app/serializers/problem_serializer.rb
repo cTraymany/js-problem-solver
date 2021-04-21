b0634d264bba2cd684ba27c47b0c61089f4a219c
Chindalath <chindalatht@gmail.com>
2021-04-21 14:18:52 -0700

    adds user key to problem serializer

--- js-problem-solver-backend/app/serializers/problem_serializer.rb
+++ js-problem-solver-backend/app/serializers/problem_serializer.rb
@@ -1,6 +1,6 @@
 class ProblemSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :title, :description
+  attributes :title, :description, :user_id
   attributes :solutions do |s|
     SolutionSerializer.new(s.solutions).as_json["data"]
   end
