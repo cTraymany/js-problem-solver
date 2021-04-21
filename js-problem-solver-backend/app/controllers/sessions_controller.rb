7a2c21b2beb07b1598959d65aca1eec4f419a70a
Chindalath <chindalatht@gmail.com>
2021-04-21 14:41:04 -0700

    adds create action to session for login

--- js-problem-solver-backend/app/controllers/sessions_controller.rb
+++ js-problem-solver-backend/app/controllers/sessions_controller.rb
@@ -1,9 +1,10 @@
 class SessionsController < ApplicationController
-    def new
-    end
+    # def new
+    #     # not using get/new because frontend JS is rendering this for us
+    # end
 
     def create
-
+        session[:username] = User.find_by(email: params[:email]).username
     end
 
     def destroy
