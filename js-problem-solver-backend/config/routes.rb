5c4ef92cbea881ee04514c2bafa5411f5c8b4515
Chindalath <chindalatht@gmail.com>
2021-04-21 14:17:27 -0700

    creates routes for signup, login, and signout

--- js-problem-solver-backend/config/routes.rb
+++ js-problem-solver-backend/config/routes.rb
@@ -2,5 +2,14 @@ Rails.application.routes.draw do
   resources :solutions
   resources :problems
   resources :users
+
+  # get 'login' => 'sessions#new'
+  # get 'signup' => 'users#new'
+  # 'get' actions will be rendered in JS since there is no view in this rails application.
+
+  post 'login' => 'sessions#create'
+  post 'signup' => 'users#create'
+  post 'logout' => 'sessions#destroy'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
