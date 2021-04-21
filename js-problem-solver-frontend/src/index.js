56ff16021e5d813074c6081c85010cb2ba6ac9fc
Chindalath <chindalatht@gmail.com>
2021-04-21 13:08:45 -0700

    comments instructions

--- js-problem-solver-frontend/src/index.js
+++ js-problem-solver-frontend/src/index.js
@@ -1,13 +1,4 @@
+// If user is logged in, 
 Problem.renderProblemForm()
 Problem.fetchProblems()
-
-// const button = document.createElement("button")
-// button.innerHTML = "Change to Yellow"
-
-// document.body.appendChild(button)
-// button.setAttribute("class", "btn-primary")
-
-// button.addEventListener("click", changeBackgroundColor)
-// function changeBackgroundColor() {
-//     document.body.style.backgroundColor = "yellow"
-// }
+// else, load app.js that leads to login/signup page
