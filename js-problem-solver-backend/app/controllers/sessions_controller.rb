cb185bc95b470147534d093cf7055a9978fda1aa
Chindalath <chindalatht@gmail.com>
2021-04-20 20:31:12 -0700

    sets up sessions controller

---
+++ js-problem-solver-backend/app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+    def new
+    end
+
+    def create
+
+    end
+
+    def destroy
+
+    end
+
+    # def google_login
+
+    # end
+
+    # private
+
+    # def auth_hash
+
+    # end
+end
