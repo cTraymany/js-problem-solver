67a2f17074b3faa67f615472c580d4d37db70050
Chindalath <chindalatht@gmail.com>
2021-04-21 14:39:52 -0700

    simplifies user problems and solutions attributes

--- js-problem-solver-backend/app/serializers/user_serializer.rb
+++ js-problem-solver-backend/app/serializers/user_serializer.rb
@@ -1,11 +1,5 @@
 class UserSerializer
     include FastJsonapi::ObjectSerializer
-    attributes :username, :email, :password
-    attributes :problems do |p|
-        ProblemSerializer.new(p.problems).as_json["data"]
-    end
-    attributes :solutions do |s|
-        SolutionSerializer.new(s.solutions).as_json["data"]
-    end
+    attributes :username, :email, :password, :problems, :solutions
   end
   
