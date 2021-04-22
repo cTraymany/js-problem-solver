cee7734f4656ef42f6ce66b29eaf5a4431ed88df
Chindalath <chindalatht@gmail.com>
2021-04-22 13:05:27 -0700

    adds ids to signup and login forms, and adds event listener for login functionality

--- js-problem-solver-frontend/src/session.js
+++ js-problem-solver-frontend/src/session.js
@@ -3,10 +3,10 @@ function loadLoginScreen() {
         <div class="login-container">
             <form id="login-form">
                 <label>Email: </label>
-                <input type="text" name="email"></input><br>
+                <input type="text" name="email" id="login-email"></input><br>
 
                 <label>Password: </label>
-                <input type="password" name="password"></input><br>
+                <input type="password" name="password" id="login-password"></input><br>
 
                 <input type="submit" value="Login"></input>
             </form>
@@ -26,12 +26,41 @@ function loadLoginScreen() {
             </form>
         </div>
     `
+
     const loginForm = document.getElementById("login-form")
 
     loginForm.addEventListener("submit", (event) => {
         event.preventDefault()
+        const email = document.getElementById("login-email").value
+        const password = document.getElementById("login-password").value
+        event.target.reset()
 
+        const jsObj = {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              "Accept": "application/json"
+            },
+            body: JSON.stringify({user: {email: email, password: password}})
+          }
+
+        // example fetch below; example jsObj above
+        fetch("http://localhost:3000/login", jsObj)
+            .then(resp => resp.json())
+            .then(jsObj => {
+                console.log(jsObj)
+                container.innerHTML = ""
+                Problem.renderProblemForm()
+                Problem.fetchProblems()
+                // add welcome message to display user information.
+                // if user is already logged in, redirect to another page
+            })
+            .catch( error => {
+                console.log(error)
+                alert("Please enter a valid login.")
+            })
     })
+
 
     // TODO: when the login form is submit
     // you must create a new user session and navigate to the problems page.
