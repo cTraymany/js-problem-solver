dbf8244cd7e41c5464709c4b7e7e2a749e8f7cd7
Chindalath <chindalatht@gmail.com>
2021-04-22 13:03:05 -0700

    this just displays the login form so it's easier to work on

--- js-problem-solver-frontend/src/index.js
+++ js-problem-solver-frontend/src/index.js
@@ -1,4 +1,5 @@
 // If user is logged in, 
-Problem.renderProblemForm()
-Problem.fetchProblems()
+// Problem.renderProblemForm()
+// Problem.fetchProblems()
 // else, load app.js that leads to login/signup page
+loadLoginScreen()
