399b02a7fff66e2c171ec0e83c78f03e93452d2f
Chindalath <chindalatht@gmail.com>
2021-04-22 13:02:36 -0700

    uncomments password validation

--- js-problem-solver-backend/app/models/user.rb
+++ js-problem-solver-backend/app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
     
     has_secure_password
 
-    validates :username, :email, presence: true #:password, presence: true
+    validates :username, :email, :password, presence: true
     validates :username, :email, uniqueness: true
     validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
