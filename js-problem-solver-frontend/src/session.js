eaabc6aaca5899cc1010f8121228fcc6ed4a5d68
Chindalath <chindalatht@gmail.com>
2021-04-22 20:35:26 -0700

    adds sign up functionality

--- js-problem-solver-frontend/src/session.js
+++ js-problem-solver-frontend/src/session.js
@@ -11,9 +11,10 @@ function loadLoginScreen() {
         <div>
             <form id="signup-form">
                 <label>Sign Up</label><br>
-                <input type="text" name="username" placeholder="Username"></input><br>
-                <input type="text" name="email" placeholder="Email"></input><br>
-                <input type="password" name="password" placeholder="Password"></input><br>
+                <input type="text" name="username" placeholder="Username" id="signup-username"></input><br>
+                <input type="text" name="email" placeholder="Email" id="signup-email"></input><br>
+                <input type="password" name="password" placeholder="Password" id="signup-password"></input><br>
+                <input type="password" name="password-confirmation" placeholder="Confirm Password" id="signup-password-confirmation"></input><br>
                 
                 <input type="submit" value="Sign Up"></input>
             </form>
@@ -37,16 +38,17 @@ function loadLoginScreen() {
             body: JSON.stringify({user: {email: email, password: password}})
           }
 
-        // example fetch below; example jsObj above
         fetch("http://localhost:3000/login", jsObj)
             .then(resp => resp.json())
             .then(jsObj => {
                 console.log(jsObj)
-                container.innerHTML = ""
-                Problem.renderProblemForm()
-                Problem.fetchProblems()
-                // add welcome message to display user information.
-                // if user is already logged in, redirect to another page
+                // if jsObj has an id, do the following.
+                    container.innerHTML = ""
+                    Problem.renderProblemForm()
+                    Problem.fetchProblems()
+                    // add welcome message to display user information.
+                    // if user is already logged in, redirect to another page
+                // end
             })
             .catch( error => {
                 console.log(error)
@@ -57,12 +59,38 @@ function loadLoginScreen() {
     const signupForm = document.getElementById("signup-form")
 
     signupForm.addEventListener("submit", (event) => {
-            event.preventDefault()
-            console.log("Create a new user!")
+        event.preventDefault()
+        const username = document.getElementById("signup-username").value
+        const email = document.getElementById("signup-email").value
+        const password = document.getElementById("signup-password").value
+        const passwordConfirmation = document.getElementById("signup-password-confirmation").value
+        event.target.reset()
+
+        const jsObj = {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              "Accept": "application/json"
+            },
+            body: JSON.stringify({user: {username: username, email: email, password: password, password_confirmation: passwordConfirmation}})
+        }
+
+        fetch("http://localhost:3000/signup", jsObj)
+        .then(resp => resp.json())
+        .then(jsObj => {
+            console.log(jsObj)
+            container.innerHTML = ""
+            Problem.renderProblemForm()
+            Problem.fetchProblems()
+            // add welcome message to display user information.
+            // if user is already logged in, redirect to another page
+        })
+        .catch( error => {
+            console.log(error)
+            alert("Please enter a valid login.")
+        })
     })
 
     // TODO: when the login form is submit
-    // you must create a new user session and navigate to the problems page.
     // add an event listener to the sign up form to create a new user.
-    // set up user#create and session#create actions in rails.
 }
