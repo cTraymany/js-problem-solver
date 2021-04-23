8fb4352f7fb331bed82205ba69a501a449dca76b
Chindalath <chindalatht@gmail.com>
2021-04-22 20:37:13 -0700

    adds create action for new users

--- js-problem-solver-backend/app/controllers/users_controller.rb
+++ js-problem-solver-backend/app/controllers/users_controller.rb
@@ -1,8 +1,23 @@
 class UsersController < ApplicationController
     def index
         render json: UserSerializer.new(User.all)
+        # delete this action. made only for testing/easy view of user objects
     end
     
-    def create 
+    def create
+        user = User.create(user_params)
+        if user.save
+            session[:user_id] = user.id
+            render json: UserSerializer.new(user)
+            # in the front end, use the render to display a welcome msg
+        else
+            render json: {errors: "Unable to create user."}
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password, :password_confirmation)
     end
 end
