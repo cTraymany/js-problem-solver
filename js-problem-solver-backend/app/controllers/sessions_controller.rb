fc49a281565dd9485c32ddb2c31f9c8488323d48
Chindalath <chindalatht@gmail.com>
2021-04-22 20:38:06 -0700

    cleans comments and changes session hash to hold user id instead of username

--- js-problem-solver-backend/app/controllers/sessions_controller.rb
+++ js-problem-solver-backend/app/controllers/sessions_controller.rb
@@ -1,16 +1,13 @@
 class SessionsController < ApplicationController
-    # def new
-    #     # not using get/new because frontend JS is rendering this for us
-    # end
-
     def create
         user = User.find_by(email: params[:user][:email])
         if user && user.authenticate(params[:user][:password])
-            session[:username] = user.username
-            render json: user         
+            session[:user_id] = user.id
+            # session[:username] = user.username
+            render json: user
         else             
             render json: {errors: "Unable to authenticate user."}        
-        end 
+        end
     end
 
     def destroy
