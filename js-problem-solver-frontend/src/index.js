baa0753cf0dc3ed856d2c42cbc1472602587a54a
Chindalath <chindalatht@gmail.com>
2021-04-27 14:31:56 -0700

    if user is logged in, renders the problems

--- js-problem-solver-frontend/src/index.js
+++ js-problem-solver-frontend/src/index.js
@@ -3,3 +3,9 @@
 // Problem.fetchProblems()
 // else, load app.js that leads to login/signup page
 loadLoginScreen()
+console.log(localStorage)
+// if there is a local storage, navigate to problems.
+if (localStorage.loggedIn) {
+    container.innerHTML = ""
+    Problem.start()
+}
