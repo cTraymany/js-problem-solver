1fefd4acebf3c8e8c90176f82626267c399e4dc2
Chindalath <chindalatht@gmail.com>
2021-04-27 11:19:51 -0700

    auth token config

--- js-problem-solver-backend/app/controllers/application_controller.rb
+++ js-problem-solver-backend/app/controllers/application_controller.rb
@@ -1,5 +1,3 @@
-class ApplicationController < ActionController::API
-# class ApplicationController < ActionController::Base
-#     protect_from_forgery with: :exception
-#     skip_before_action :verify_authenticity_token
+class ApplicationController < ActionController::Base
+    skip_before_action :verify_authenticity_token
 end
