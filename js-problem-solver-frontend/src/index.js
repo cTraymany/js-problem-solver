4d9e96c9527fcb4b7070976de353a91638f7dec6
Chindalath <chindalatht@gmail.com>
2021-04-27 17:02:47 -0700

    if user is logged in, renders problems, otherwise renders landing page

--- js-problem-solver-frontend/src/index.js
+++ js-problem-solver-frontend/src/index.js
@@ -1,20 +1,5 @@
-// If user is logged in, 
-// Problem.renderProblemForm()
-// Problem.fetchProblems()
-// else, load app.js that leads to login/signup page
-loadLoginScreen()
-console.log(localStorage)
-// if there is a local storage, navigate to problems.
 if (localStorage.loggedIn) {
-    container.innerHTML = ""
     Problem.start()
+} else {
+    loadLandingPage()
 }
-
-// // INCOMING CHANGE BELOW
-// const getStarted = document.getElementById("get-started")
-// getStarted.addEventListener("click", Problem.start)
-// // start app on click for now, but nav to signup page once auth is set up
-
-// const solveAProblem = document.getElementById("solve-a-problem")
-// solveAProblem.addEventListener("click", Problem.start)
-// // start app on click for now, but nav to signup page once auth is set up
