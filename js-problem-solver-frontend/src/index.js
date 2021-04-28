3a08a5262be94a6edf86e273437e981c43fb70b1
Chindalath <chindalatht@gmail.com>
2021-04-27 19:11:35 -0700

    adds event listener to logo on click to display a new page dependent on login status

--- js-problem-solver-frontend/src/index.js
+++ js-problem-solver-frontend/src/index.js
@@ -3,3 +3,24 @@ if (localStorage.loggedIn) {
 } else {
     loadLandingPage()
 }
+
+const logo = document.getElementById("logo")
+
+logo.addEventListener("click", event => {
+    event.preventDefault()
+    document.body.style.background = ""
+
+    if (localStorage.loggedIn) {
+        document.body.style.background = "pink"
+        console.log("load the welcome page!")
+    } else {
+        loadLandingPage()
+    }
+})
+
+// window.addEventListener("load", event => localStorage.setItem("fetched", false))
+
+// if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {
+//     localStorage.setItem("fetched", false)
+//     console.log(localStorage.fetched)
+// }
