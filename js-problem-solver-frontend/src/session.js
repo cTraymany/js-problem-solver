e89e0f008639ba1025a5fa96b792c16b70fc11a0
Chindalath <chindalatht@gmail.com>
2021-04-27 17:20:54 -0700

    put load of landing page into its own function in session. changes visibility of buttons to be dependent on login. fixes bug in login so that problems are not rendering multiple times.

--- js-problem-solver-frontend/src/session.js
+++ js-problem-solver-frontend/src/session.js
@@ -1,3 +1,79 @@
+function loadLandingPage() {
+    container.innerHTML = `
+        <header class="hero-container">
+            <div class="right">
+                <h2>We all have problems. Let us solve yours!</h2>
+                <a class="btn" id="get-started" href=#>Get Started</a>
+            </div>
+            <img src="./img/pexels-nathan-cowley-897817.png">
+        </header>
+        <!-- end of hero section -->
+        <section class="selling-points-container">
+            <h3>We're here to get you there</h3>
+            <ul class="selling-points">
+                <li>
+                    <h4>Easy to use</h4>
+                    <p>Solving problems  has<br>never been so easy. Get started in minutes!</p>
+                    <div class="line"></div>
+                </li>
+                <li>
+                    <h4>You're protected</h4>
+                    <p>All problems are<br>anonymous to ensure complete privacy.</p>
+                    <div class="line"></div>
+                </li>
+                <li>
+                    <h4>Try for free</h4>
+                    <p>Solve your first problem for free, there are no hidden fees. Yes—we’re serious.</p>
+                </li>
+            </ul>
+        </section>
+        <section class="testimonials-container">
+            <h3>See what people are saying about us</h3>
+            <ul class="testimonials">
+                <li>
+                    <img src="img/pexels-háº£i-nguyá»_n-5321674.png">
+                    <div>
+                        <h5>Cindy S</h5>
+                        <p>“Yooooo... I had a problem and now it’s solved.”</p>
+                    </div>
+                </li>
+                <li>
+                    <img src="img/eduardo-dutra-614571-unsplash.png">
+                    <div>
+                        <h5>Rahim H</h5>
+                        <p>“Wow. I’m rich now! Are you serious bro? Problem Solver rocks!!”</p>
+                    </div>
+                </li>
+                <li>
+                    <img src="img/jan-abellan-Vf6Voxf-q80-unsplash.png">
+                    <div>
+                        <h5>John A</h5>
+                        <p>“That war never happened. Thank you.”</p>
+                    </div>
+                </li>
+            </ul>
+        </section>
+        <section class="call-to-action">
+            <h3>Let's do this.</h3>
+            <a class="btn" id="solve-a-problem" href=#>Solve a problem</a>
+        </section>
+    `
+    const loginButton = document.getElementById("login-button")
+    const signupButton = document.getElementById("signup-button")
+    loginButton.classList.remove("inactive")
+    signupButton.classList.remove("inactive")
+
+    loginButton.addEventListener("click", event => loadLoginScreen())
+    // signupButton.addEventListener("click", event => loadSignupScreen())
+
+    // // INCOMING CHANGE BELOW
+    // const getStarted = document.getElementById("get-started")
+    // getStarted.addEventListener("click", event => loadSignupPage())
+
+    // const solveAProblem = document.getElementById("solve-a-problem")
+    // solveAProblem.addEventListener("click", event => loadSignupPage())
+}
+
 function loadLoginScreen() {
     container.innerHTML = `
         <div class="login-container">
@@ -20,6 +96,12 @@ function loadLoginScreen() {
             </form>
         </div>
     `
+    document.body.style.background = "cadetblue"
+
+    const loginButton = document.getElementById("login-button")
+    const signupButton = document.getElementById("signup-button")
+    loginButton.classList.add("inactive")
+    signupButton.classList.add("inactive")
 
     const loginForm = document.getElementById("login-form")
 
@@ -48,10 +130,10 @@ function loadLoginScreen() {
                     localStorage.setItem("userId", jsObj.session.id)
                     localStorage.setItem("loggedIn", true)
                     console.log(localStorage)
-                    
+
                     container.innerHTML = ""
-                    Problem.renderProblemForm()
-                    Problem.fetchProblems()
+
+                    Problem.start()
                     const userId = jsObj.session.id
                     // add welcome message to display user information.
                     // if user is already logged in, redirect to another page
