e53b24eab734552b3216f141c6359c003e0cb01c
Chindalath <chindalatht@gmail.com>
2021-04-27 19:14:30 -0700

    separates signup and login into two separate pages, and adds ability for new user to sign up.

--- js-problem-solver-frontend/src/session.js
+++ js-problem-solver-frontend/src/session.js
@@ -60,68 +60,127 @@ function loadLandingPage() {
     `
     const loginButton = document.getElementById("login-button")
     const signupButton = document.getElementById("signup-button")
+    const getStarted = document.getElementById("get-started")
+    const solveAProblem = document.getElementById("solve-a-problem")
+
     loginButton.classList.remove("inactive")
     signupButton.classList.remove("inactive")
 
-    loginButton.addEventListener("click", event => loadLoginScreen())
-    // signupButton.addEventListener("click", event => loadSignupScreen())
-
-    // // INCOMING CHANGE BELOW
-    // const getStarted = document.getElementById("get-started")
-    // getStarted.addEventListener("click", event => loadSignupPage())
-
-    // const solveAProblem = document.getElementById("solve-a-problem")
-    // solveAProblem.addEventListener("click", event => loadSignupPage())
+    loginButton.addEventListener("click", event => loadLoginPage())
+    signupButton.addEventListener("click", event => loadSignupPage())
+    getStarted.addEventListener("click", event => loadSignupPage())
+    solveAProblem.addEventListener("click", event => loadSignupPage())
 }
 
-function loadLoginScreen() {
-    container.innerHTML = `
-        <div class="login-container">
-            <form id="login-form">
-                <label>Login</label><br>
-                <input type="text" name="email" placeholder="Email" id="login-email"></input><br>
-                <input type="password" name="password" placeholder="Password" id="login-password"></input><br>
-                <input type="submit" value="Login"></input>
-            </form>
-        </div>
-        <div>
-            <form id="signup-form">
-                <label>Sign Up</label><br>
-                <input type="text" name="username" placeholder="Username" id="signup-username"></input><br>
-                <input type="text" name="email" placeholder="Email" id="signup-email"></input><br>
-                <input type="password" name="password" placeholder="Password" id="signup-password"></input><br>
-                <input type="password" name="password-confirmation" placeholder="Confirm Password" id="signup-password-confirmation"></input><br>
-                
-                <input type="submit" value="Sign Up"></input>
-            </form>
-        </div>
-    `
-    document.body.style.background = "cadetblue"
+function loadLoginPage() {
+        container.innerHTML = `
+            <div class="login-container">
+                <form id="login-form">
+                    <label>Login</label><br>
+                    <input type="text" name="email" placeholder="Email" id="login-email"></input><br>
+                    <input type="password" name="password" placeholder="Password" id="login-password"></input><br>
+                    <input type="submit" value="Login"></input>
+                </form>
+            </div>
+        `
+        document.body.style.background = "cadetblue"
+    
+        const loginButton = document.getElementById("login-button")
+        const signupButton = document.getElementById("signup-button")
+        loginButton.classList.add("inactive")
+        signupButton.classList.add("inactive")
+    
+        const loginForm = document.getElementById("login-form")
+    
+        loginForm.addEventListener("submit", (event) => {
+            event.preventDefault()
+            const email = document.getElementById("login-email").value
+            const password = document.getElementById("login-password").value
+            event.target.reset()
+    
+            const jsObj = {
+                method: "POST",
+                credentials: "same-origin",
+                headers: {
+                  "Content-Type": "application/json",
+                  "Accept": "application/json"
+                },
+                body: JSON.stringify({user: {email: email, password: password}})
+              }
+    
+            fetch("http://localhost:3000/login", jsObj)
+                .then(resp => resp.json())
+                .then(jsObj => {
+                    console.log(jsObj)
+                    if (jsObj.logged_in) {
+                        localStorage.setItem("username", jsObj.session.username)
+                        localStorage.setItem("userId", jsObj.session.id)
+                        localStorage.setItem("loggedIn", true)
+                        console.log(localStorage)
+    
+                        container.innerHTML = ""
+    
+                        Problem.start()
+                        // ^^ONCE LOGGED IN, LOAD THE WELCOME SCREEN INSTEAD OF NAVIGATING TO PROBLEMS RIGHT AWAY
+                        // const userId = jsObj.session.id
+                        // ^^what is this variable set for???
+                        // add welcome message to display user information.
+                    } else {
+                        console.log(error)
+                        alert("Please enter a valid login.")
+                        // change alert to render something prettier
+    
+                    }
+                })
+                .catch( error => {
+                    console.log(error)
+                    alert("Please enter a valid login.")
+                })
+        })
+}
 
-    const loginButton = document.getElementById("login-button")
-    const signupButton = document.getElementById("signup-button")
-    loginButton.classList.add("inactive")
-    signupButton.classList.add("inactive")
-
-    const loginForm = document.getElementById("login-form")
-
-    loginForm.addEventListener("submit", (event) => {
-        event.preventDefault()
-        const email = document.getElementById("login-email").value
-        const password = document.getElementById("login-password").value
-        event.target.reset()
-
-        const jsObj = {
-            method: "POST",
-            credentials: "same-origin",
-            headers: {
-              "Content-Type": "application/json",
-              "Accept": "application/json"
-            },
-            body: JSON.stringify({user: {email: email, password: password}})
-          }
-
-        fetch("http://localhost:3000/login", jsObj)
+function loadSignupPage() {
+        container.innerHTML = `
+            <div>
+                <form id="signup-form">
+                    <label>Sign Up</label><br>
+                    <input type="text" name="username" placeholder="Username" id="signup-username"></input><br>
+                    <input type="text" name="email" placeholder="Email" id="signup-email"></input><br>
+                    <input type="password" name="password" placeholder="Password" id="signup-password"></input><br>
+                    <input type="password" name="password-confirmation" placeholder="Confirm Password" id="signup-password-confirmation"></input><br>
+                    
+                    <input type="submit" value="Sign Up"></input>
+                </form>
+            </div>
+        `
+    
+        document.body.style.background = "cadetblue"
+    
+        const loginButton = document.getElementById("login-button")
+        const signupButton = document.getElementById("signup-button")
+        loginButton.classList.add("inactive")
+        signupButton.classList.add("inactive")
+    
+        const signupForm = document.getElementById("signup-form")
+    
+        signupForm.addEventListener("submit", (event) => {
+            event.preventDefault()
+            const username = document.getElementById("signup-username").value
+            const email = document.getElementById("signup-email").value
+            const password = document.getElementById("signup-password").value
+            const passwordConfirmation = document.getElementById("signup-password-confirmation").value
+            event.target.reset()
+    
+            const jsObj = {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                  "Accept": "application/json"
+                },
+                body: JSON.stringify({user: {username: username, email: email, password: password, password_confirmation: passwordConfirmation}})
+            }
+    
+            fetch("http://localhost:3000/signup", jsObj)
             .then(resp => resp.json())
             .then(jsObj => {
                 console.log(jsObj)
@@ -130,59 +189,23 @@ function loadLoginScreen() {
                     localStorage.setItem("userId", jsObj.session.id)
                     localStorage.setItem("loggedIn", true)
                     console.log(localStorage)
-
+                    
                     container.innerHTML = ""
-
+                    
                     Problem.start()
-                    const userId = jsObj.session.id
+                    // ^^ONCE LOGGED IN, LOAD THE WELCOME SCREEN INSTEAD OF NAVIGATING TO PROBLEMS RIGHT AWAY
+                    // const userId = jsObj.session.id
+                    // ^^what is this variable set for???
                     // add welcome message to display user information.
-                    // if user is already logged in, redirect to another page
                 } else {
                     console.log(error)
-                    alert("Please enter a valid login.")
+                    alert("Cannot create user.")
+                    // change alert to render something prettier
                 }
             })
             .catch( error => {
                 console.log(error)
                 alert("Please enter a valid login.")
             })
-    })
-
-    const signupForm = document.getElementById("signup-form")
-
-    signupForm.addEventListener("submit", (event) => {
-        event.preventDefault()
-        const username = document.getElementById("signup-username").value
-        const email = document.getElementById("signup-email").value
-        const password = document.getElementById("signup-password").value
-        const passwordConfirmation = document.getElementById("signup-password-confirmation").value
-        event.target.reset()
-
-        const jsObj = {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              "Accept": "application/json"
-            },
-            body: JSON.stringify({user: {username: username, email: email, password: password, password_confirmation: passwordConfirmation}})
-        }
-
-        fetch("http://localhost:3000/signup", jsObj)
-        .then(resp => resp.json())
-        .then(jsObj => {
-            console.log(jsObj)
-            container.innerHTML = ""
-            Problem.renderProblemForm()
-            Problem.fetchProblems()
-            // add welcome message to display user information.
-            // if user is already logged in, redirect to another page
         })
-        .catch( error => {
-            console.log(error)
-            alert("Please enter a valid login.")
-        })
-    })
-
-    // TODO: when the login form is submit
-    // add an event listener to the sign up form to create a new user.
 }
