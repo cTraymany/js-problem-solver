8d9b2e35da22b4258f1e9a07b5c6196d71008613
Chindalath <chindalatht@gmail.com>
2021-05-02 11:38:09 -0700

    comments possible config origin

--- js-problem-solver-backend/config/initializers/cors.rb
+++ js-problem-solver-backend/config/initializers/cors.rb
@@ -8,6 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
+    # origins /\Alocalhost:3000\/\w*\/?\d*\z/
 
     resource '*',
       headers: :any,
