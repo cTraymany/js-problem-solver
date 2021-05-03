852e9e24f7c9ef812c74ee2ca2adda821fbf0d13
Chindalath <chindalatht@gmail.com>
2021-05-02 18:44:30 -0700

    disables origin check, for testing purposes

--- js-problem-solver-backend/config/application.rb
+++ js-problem-solver-backend/config/application.rb
@@ -24,6 +24,11 @@ module JsProblemSolverBackend
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # Here, we turn off forgery protection origin check so we can make
+    # post requests from a local file instead of a domain.
+    # Remove this line of code before deploying.
+    config.action_controller.forgery_protection_origin_check = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
