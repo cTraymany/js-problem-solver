93793eeecb799b7868801669c7a419d26c11310c
Chindalath <chindalatht@gmail.com>
2021-09-10 15:33:17 -0700

    solutions route

--- js-problem-solver-backend/app/controllers/solutions_controller.rb
+++ js-problem-solver-backend/app/controllers/solutions_controller.rb
@@ -1,4 +1,9 @@
 class SolutionsController < ApplicationController
+
+    def index
+        render json: SolutionSerializer.new(Solution.all)
+    end
+
     def create
         solution = Solution.create(solution_params)
 
